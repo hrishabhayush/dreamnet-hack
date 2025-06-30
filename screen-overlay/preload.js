4f28a113545985356fa82edbb928583aca2a6a2e
Hrishabh Ayush <hrishabh.ay@gmail.com>
2025-06-30 12:22:53 -0400

    screen-overlay updated with renderer and preload

---
+++ screen-overlay/preload.js
@@ -0,0 +1,7 @@
+const { contextBridge, ipcRenderer } = require('electron/renderer')
+
+contextBridge.exposeInMainWorld('versions', {
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron
+})
